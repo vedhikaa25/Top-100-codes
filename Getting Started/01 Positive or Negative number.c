b30119c0bf0a4afef0f03b1c8fb6bb4e91cd75fb
Vedhikaa <vedhikaa25@gmail.com>
2024-08-24 13:23:48 +0530

    Update 01 Positive or Negative number.c

--- Getting Started/01 Positive or Negative number.c
+++ Getting Started/01 Positive or Negative number.c
@@ -1,1 +1,13 @@
-!
+link -> [https://prepinsta.com/c-program/a-number-is-positive-or-negative/]
+#include<stdio.h> 
+
+int main(){
+  int n = 5;
+  if(n>=0){
+    printf("Positive");
+  }
+  else{
+    printf("Negative");
+}
+  return 0;
+}
